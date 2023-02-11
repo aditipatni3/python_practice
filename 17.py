693ed5e411ccaaa5344559789c8fa8a5dee0f2ad
aditipatni <aditipatni311@gmail.com>
2023-02-11 17:58:28 +0530

    demo

---
+++ 17.py
@@ -0,0 +1,12 @@
+#dictionaries
+
+dic1={"name":"aditi", "age":21, "Indian":True}
+print(dic1)
+print(dic1.keys())   #to print keys
+print(dic1.values()) #to print values
+
+print(dic1["name"])
+
+#to check if key exists
+print(dic1.get('eligible'))
+print(dic1.get('Indian'))
