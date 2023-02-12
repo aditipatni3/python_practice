3831ba9ceb8ef7ed97602fec4c6db46e8cea8540
aditipatni <aditipatni311@gmail.com>
2023-02-12 21:09:20 +0530

    demo

--- 17.py
+++ 17.py
@@ -1,12 +1,22 @@
 #dictionaries
 
 dic1={"name":"aditi", "age":21, "Indian":True}
-print(dic1)
-print(dic1.keys())   #to print keys
-print(dic1.values()) #to print values
+# print(dic1)
+# print(dic1.items())
+# print(dic1.keys())   #to print keys
+# print(dic1.values()) #to print values
 
-print(dic1["name"])
+# print(dic1["name"])
 
-#to check if key exists
-print(dic1.get('eligible'))
-print(dic1.get('Indian'))
+# #to check if key exists
+# print(dic1.get('eligible'))
+# print(dic1.get('Indian'))
+
+print(dic1.items())
+#iteration 
+for key, value in dic1.items():
+    print(f"the value corresponding to the key {key} is {value}")  #f-string
+    
+#dictionary methods
+
+
