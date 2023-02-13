f989eb4995a5637267787ae0dd1998b6234731a1
aditipatni <aditipatni311@gmail.com>
2023-02-13 21:18:46 +0530

    demo

--- 17.py
+++ 17.py
@@ -19,4 +19,36 @@ for key, value in dic1.items():
     
 #dictionary methods
 
+#update
+dic1.update({"age":22})
+print(dic1)
 
+dic1.update({"dob":3})
+print(dic1)
+
+dic2={"hello":"world", "my":"name"}
+dic1.update(dic2)
+print(dic1)
+
+#clear()
+dic2.clear()
+print(dic2)
+
+
+#to print empty dic
+dic={}
+print(dic)
+
+#to pop
+dic1.pop("hello")
+print(dic1)
+
+dic1.popitem()  # removes the last key value pair
+print(dic1)
+
+#del
+del dic1['age']
+print(dic1) #if something mentioned with dic-  it oly deletes the mentioned key value pair
+
+del dic1
+print(dic1)  #it deletes the entire dic
