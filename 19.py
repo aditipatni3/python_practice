ffa3f7e7735980f9315bca5a8d70c31a3283fd53
aditipatni <aditipatni311@gmail.com>
2023-02-14 22:36:18 +0530

    demo

--- 19.py
+++ 19.py
@@ -3,7 +3,7 @@ a=int(input("enter the value of a:"))
 try:
     for i in range(1,11):
         print(f"int{a}x{i}=int({a*i})")
-except:
-    print("error as ")
+except Exception as e:
+    print("error as ", e)
     
 print("👍")
