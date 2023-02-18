7214eb6418838653f34fa2d34eb377f92263a1bb
aditipatni <aditipatni311@gmail.com>
2023-02-18 23:24:32 +0530

    demo

---
+++ 23.py
@@ -0,0 +1,24 @@
+#verifying a phone number using regex
+import re
+phn="313-414-5656"
+if re.search("\w{3}-\w{3}-\w{4}", phn):
+    print("it is a phone number")
+else:
+    print("the number isn't a phone a number")
+    
+    
+phn="313-4145-5656"
+if re.search(r"\b\w{3}-\w{3}-\w{4}\b", phn):
+    print("it is a phone number")
+else:
+    print("the number isn't a phone number")
+    
+    
+    
+phn="3134-414-5656"
+if re.search(r"\b\w{3}-\w{3}-\w{4}\b", phn):
+    print("it is a phone number")
+else:
+    print("the number isn't a phone number")
+    
+    
