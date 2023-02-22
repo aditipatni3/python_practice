779276e48db03cd174755e3a594dd01c7b428f12
aditipatni <aditipatni311@gmail.com>
2023-02-22 19:56:18 +0530

    demo

--- 19.py
+++ 19.py
@@ -1,9 +1,9 @@
 #exception handling
-a=int(input("enter the value of a:"))
+a=(input("enter the value of a:"))
 try:
     for i in range(1,11):
-        print(f"int{a}x{i}=int({a*i})")
-except Exception as e:
-    print("error as ", e)
+        print(f"{int(a)}x{i}={int(a)}*i)")
+except:
+    print("error ")
     
 print("💕")
