c9eda9cbcba029c9bf42df792a3bfb687b1606f8
aditipatni <aditipatni311@gmail.com>
2023-03-27 17:16:14 +0530

    demo

---
+++ 25.py
@@ -0,0 +1,20 @@
+#exam
+
+#precedence of operators
+print(2**3**2)
+
+print(2**9)
+#print(8**2)\
+    
+    
+#equality test by == and !=
+x=20
+y=21
+print(x==y)
+
+#identity test by is and is not
+# x=20
+# y=20
+# print(id(x))
+# print(id(y))
+# print(id(x) is  id(y))
